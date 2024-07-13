7ab4ed9bda8e9c3687ecae068e47440738a52c0f
Uttkarsh <uttkarsh.a.mishra@gmail.com>
2024-07-14 01:18:13 +0530

    commited

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to my React App</h1>
+        <p>This is my first React Application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
